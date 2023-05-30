d16c4ff66499dcef24f8fd29f0b121e3ab8ac107
powerninja <asdyuy23@gmail.com>
2023-05-31 00:02:46 +0900

    add:sendを追加

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.listen(5000, function () {
+app.get('/', function (req, res) {
+  res.send('こんにちは');
+});
+
+app.listen(5050, function () {
   console.log('listeing on localhost port 5000');
 });
