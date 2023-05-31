7e4342b59ffd757c02f065fbedbbd864a5d136f2
powerninja <asdyuy23@gmail.com>
2023-06-01 00:09:24 +0900

    add:form追加&フォームに対応するPOSTメソッドの作成

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.post('/autumn', function (req, res) {});
+
 app.listen(5050, function () {
   console.log('listening on localhost port 5050');
 });
